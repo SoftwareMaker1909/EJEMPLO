2a7bdcc9beb82249fc471ec18731a81dbd06c86b
SoftwareMaker1909 <105822309+SoftwareMaker1909@users.noreply.github.com>
2023-11-27 17:44:52 -0400

    temperatura.py V1.0

--- Convertir temperatura.py
+++ Convertir temperatura.py
@@ -1,3 +1,4 @@
+
 def celsius_a_fahrenheit(celsius):
     fahrenheit = (celsius * 9/5) + 32
     return fahrenheit
@@ -5,3 +6,20 @@ def celsius_a_fahrenheit(celsius):
 def fahrenheit_a_celsius(fahrenheit):
     celsius = (fahrenheit - 32) * 5/9
     return celsius
+
+def convertir_temperatura():
+    print("¿Qué conversión desea realizar?")
+    print("1. Celsius a Fahrenheit")
+    print("2. Fahrenheit a Celsius")
+    opcion = int(input("Ingrese una opción: "))
+    temperatura = float(input("Ingrese la temperatura: "))
+    if opcion == 1:
+        resultado = celsius_a_fahrenheit(temperatura)
+        print(f"{temperatura} grados Celsius son {resultado} grados Fahrenheit.")
+    elif opcion == 2:
+        resultado = fahrenheit_a_celsius(temperatura)
+        print(f"{temperatura} grados Fahrenheit son {resultado} grados Celsius.")
+    else:
+        print("Opción inválida.")
+
+convertir_temperatura()
