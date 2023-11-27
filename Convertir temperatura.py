5d4d34e9caa8ddf62402fc22bf4ac4089553d7f1
SoftwareMaker1909 <105822309+SoftwareMaker1909@users.noreply.github.com>
2023-11-27 17:41:59 -0400

    temperatura.py v.02

--- Convertir temperatura.py
+++ Convertir temperatura.py
@@ -1,3 +1,7 @@
 def celsius_a_fahrenheit(celsius):
     fahrenheit = (celsius * 9/5) + 32
     return fahrenheit
+
+def fahrenheit_a_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5/9
+    return celsius
